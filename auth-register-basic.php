46f8d2406037ba243c1ebdf367444281abb94fc4
thesis101graduated <vallesroselyn11ucu@gmail.com>
2024-11-06 22:21:16 +0800

    Icon ng hide password try

--- auth-register-basic.php
+++ auth-register-basic.php
@@ -161,7 +161,7 @@
                       id="password"
                       class="form-control"
                       name="password"
-                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                      placeholder="&#xb5;&#xb5;&#xb5;&#xb5;&#xb5;&#xb5;&#xb5;&#xb5;&#xb5;&#xb5;&#xb5;&#xb5;"
                       aria-describedby="password"
                     />
                     <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
