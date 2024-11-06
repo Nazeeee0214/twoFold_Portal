ffc293be09e35101f8769cb18ee1efed7eeff0e6
mirirowwwssss0 <annmisoles2003@gmail.com>
2024-11-06 16:08:43 +0800

    Update auth-register-basic.php

--- auth-register-basic.php
+++ auth-register-basic.php
@@ -78,8 +78,8 @@
                 <a href="index.php" class="app-brand-link gap-2">
                   <span class="app-brand-logo demo">
                     <svg
-                      width="50"
-                      viewBox="0 0 30 50"
+                      width="25"
+                      viewBox="0 0 25 42"
                       version="1.1"
                       xmlns="http://www.w3.org/2000/svg"
                       xmlns:xlink="http://www.w3.org/1999/xlink"
