510816a6e2ae31d2f7abfba330a5f89ef2597518
thesis101graduated <vallesroselyn11ucu@gmail.com>
2024-11-06 12:28:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 // Check if the user is logged in by checking a session variable, e.g., 'user_id'
 if (!isset($_SESSION['user_id'])) {
     // Redirect to login page
-    header("Location: auth-basic-login.php");
+    header("Location: auth-login-basic.php");
     exit();
 }
 ?>
