1f4533572bd0ae87ed4dce11282542a07597762a
mirirowwwssss0 <annmisoles2003@gmail.com>
2024-11-06 15:43:34 +0800

    update

--- auth-login-basic.php
+++ auth-login-basic.php
@@ -35,9 +35,10 @@
     <link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.ico" />
 
     <!-- Fonts -->
+    <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Anton+SC&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton+SC&display=swap" rel="stylesheet">
     />
 
     <!-- Icons. Uncomment required icon fonts -->
