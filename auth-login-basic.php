64ffa5a40016915b1aebe61060f624751d005d6d
mirirowwwssss0 <annmisoles2003@gmail.com>
2024-11-06 15:46:17 +0800

    Update auth-login-basic.php

--- auth-login-basic.php
+++ auth-login-basic.php
@@ -35,6 +35,7 @@
     <link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.ico" />
 
     <!-- Fonts -->
+    <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Anton+SC&display=swap" rel="stylesheet">
