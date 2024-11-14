db980936119d39ac413323bcaa04b0a648e80fed
akoto <jeromevillapa22@gmail.com>
2024-11-14 14:20:50 +0800

    profile
    design

--- profile.php
+++ profile.php
@@ -30,4 +30,54 @@ $tit = 'Dashboard';
       </div>
     </div>
   </div>
+  
+  <div class="container rounded bg-white mt-5 mb-5" style="margin-right: 23px; max-width: 1072px; padding: 20px;">
+    <div class="row">
+        <div class="col-md-3 border-right">
+            <div class="d-flex flex-column align-items-center text-center p-3 py-5">
+                <img class="rounded-circle mt-5" width="120px" src="https://scontent.fcrk2-4.fna.fbcdn.net/v/t1.6435-9/139249539_429946701523107_8136882659857682087_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeHtFJCik3657iAdTOPVvJfv-eeOYT3Qmcj5545hPdCZyDycPY0C_tKkXRKR-WlCEjZyMDOxxKYHKZCHdVVuO0Py&_nc_ohc=u7PuCVyJkZAQ7kNvgE7rTJd&_nc_zt=23&_nc_ht=scontent.fcrk2-4.fna&_nc_gid=AghX4y7DE-TRu8z2_RU3e1c&oh=00_AYAxbSgQMKCxBP80i28_s23lhSwcUWbXk1HUY-EafgEKZQ&oe=675CFF99">
+                <span class="font-weight-bold">Saint Joseph</span>
+                <span class="text-black-50">badgirl@mail.com.my</span>
+                <span> </span>
+            </div>
+        </div>
+        <div class="col-md-5 border-right">
+            <div class="p-3 py-5">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                    <h4 class="text-right">Profile Settings</h4>
+                </div>
+                <div class="row mt-2">
+                    <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value=""></div>
+                    <div class="col-md-6"><label class="labels">Surname</label><input type="text" class="form-control" value="" placeholder="surname"></div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-md-12"><label class="labels">Mobile Number</label><input type="text" class="form-control" placeholder="enter phone number" value=""></div>
+                    <div class="col-md-12"><label class="labels">Address Line 1</label><input type="text" class="form-control" placeholder="enter address line 1" value=""></div>
+                    <div class="col-md-12"><label class="labels">Address Line 2</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
+                    <div class="col-md-12"><label class="labels">Postcode</label><input type="text" class="form-control" placeholder="postcode" value=""></div>
+                    <div class="col-md-12"><label class="labels">State</label><input type="text" class="form-control" placeholder="state" value=""></div>
+                    <div class="col-md-12"><label class="labels">Area</label><input type="text" class="form-control" placeholder="area" value=""></div>
+                    <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control" placeholder="email id" value=""></div>
+                    <div class="col-md-12"><label class="labels">Education</label><input type="text" class="form-control" placeholder="education" value=""></div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-md-6"><label class="labels">Country</label><input type="text" class="form-control" placeholder="country" value=""></div>
+                    <div class="col-md-6"><label class="labels">State/Region</label><input type="text" class="form-control" value="" placeholder="state"></div>
+                </div>
+                <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="p-3 py-5">
+                <div class="d-flex justify-content-between align-items-center experience"><span>Edit Experience</span><span class="border px-3 p-1 add-experience"><i class="fa fa-plus"></i>&nbsp;Experience</span></div><br>
+                <div class="col-md-12"><label class="labels">Experience in Designing</label><input type="text" class="form-control" placeholder="experience" value=""></div> <br>
+                <div class="col-md-12"><label class="labels">Additional Details</label><input type="text" class="form-control" placeholder="additional details" value=""></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+</div>
+</div>
 </body>
