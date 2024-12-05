0080dce3fac79809dee462adf082fd9c8a227434
akoto <jeromevillapa22@gmail.com>
2024-12-05 19:54:26 +0800

    data manager
    nagpalit ng echarts

--- data_manager.php
+++ data_manager.php
@@ -29,17 +29,12 @@ include 'partials/_header.php' ?>
           <div class="container-xxl flex-grow-1 container-p-y">
             <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Management /</span> Bin capacity</h4>
             <div class="row">
-              <div class="col-6 ">
-                <div class="chart-container">
-                  <div id="botlcap"></div>
-                </div>
-              </div>
-              <div class="col-6">
-                <div class="chart-container">
-                  <div id="main"></div>
-                </div>
-              </div>
-            </div>
+              <div class="col-12" style="width: 97.5%;">
+  <div class="chart-container">
+    <div id="botlcap" style="width: 100%; height: 400px;"></div>
+  </div>
+</div>
+              
             <div class="row">
               <div class="col-12">
                 <div class="chart-container">
@@ -62,215 +57,119 @@ include 'partials/_header.php' ?>
 
     <script>
       // JavaScript for #main chart
-      var chartDom = document.getElementById('main');
-      var myChart = echarts.init(chartDom);
-      var option;
 
-      option = {
-        series: [{
-          type: 'gauge',
-          startAngle: 180,
-          endAngle: 0,
-          min: 0,
-          max: 100,
-          splitNumber: 10,
-          itemStyle: {
-            color: '#58D9F9',
-            shadowColor: 'rgba(0,138,255,0.45)',
-            shadowBlur: 10,
-            shadowOffsetX: 2,
-            shadowOffsetY: 2
-          },
-          progress: {
-            show: true,
-            roundCap: true,
-            width: 18
-          },
-          pointer: {
-            icon: 'path://M2090.36389,615.30999 L2090.36389,615.30999 C2091.48372,615.30999 2092.40383,616.194028 2092.44859,617.312956 L2096.90698,728.755929 C2097.05155,732.369577 2094.2393,735.416212 2090.62566,735.56078 C2090.53845,735.564269 2090.45117,735.566014 2090.36389,735.566014 L2090.36389,735.566014 C2086.74736,735.566014 2083.81557,732.63423 2083.81557,729.017692 C2083.81557,728.930412 2083.81732,728.84314 2083.82081,728.755929 L2088.2792,617.312956 C2088.32396,616.194028 2089.24407,615.30999 2090.36389,615.30999 Z',
-            length: '75%',
-            width: 16,
-            offsetCenter: [0, '5%']
-          },
-          axisLine: {
-            roundCap: true,
-            lineStyle: {
-              width: 18
-            }
-          },
-          axisTick: {
-            splitNumber: 2,
-            lineStyle: {
-              width: 2,
-              color: '#999'
-            }
-          },
-          splitLine: {
-            length: 12,
-            lineStyle: {
-              width: 3,
-              color: '#999'
-            }
-          },
-          axisLabel: {
-            distance: 30,
-            color: '#999',
-            fontSize: 12
-          },
-          title: {
-            show: false
-          },
-          detail: {
-            backgroundColor: '#fff',
-            borderColor: '#999',
-            borderWidth: 2,
-            width: '60%',
-            lineHeight: 40,
-            height: 40,
-            borderRadius: 8,
-            offsetCenter: [0, '35%'],
-            valueAnimation: true,
-            formatter: function(value) {
-              return '{value|' + value.toFixed(0) + '}{unit|%}';
-            },
-            rich: {
-              value: {
-                fontSize: 12,
-                fontWeight: 'bolder',
-                color: '#777'
-              },
-              unit: {
-                fontSize: 12,
-                color: '#999',
-                padding: [0, 0, 0, 10]
-              }
-            }
-          },
-          data: [{
-            value: 20
-          }]
-        }]
-      };
-
-      option && myChart.setOption(option);
 
       // JavaScript for #botlcap chart with percentage labels on bars
-      var chartDomBotlcap = document.getElementById('botlcap');
-      var myChartBotlcap = echarts.init(chartDomBotlcap);
-      var optionBotlcap;
+ var chartDomBotlcap = document.getElementById('botlcap');
+var myChartBotlcap = echarts.init(chartDomBotlcap);
+var optionBotlcap;
 
-      optionBotlcap = {
-        xAxis: {
-          data: ['Clear', 'Colored']
-        },
-        yAxis: {
-          max: 100 // Set max to 100 for percentage scale
-        },
-        dataGroupId: '',
-        animationDurationUpdate: 500,
-        series: {
-          type: 'bar',
-          id: 'sales',
-          data: [{
-              value: 26,
-              groupId: 'clear',
-              name: 'Clear'
-            },
-            {
-              value: 100,
-              groupId: 'colored',
-              name: 'Colored'
-            }
-          ],
-          label: {
-            show: true,
-            position: 'top',
-            formatter: '{c}%', // Display value with percentage symbol
-            color: '#555',
-            fontSize: 12
-          },
-          universalTransition: {
-            enabled: true,
-            divideShape: 'clone'
-          }
+optionBotlcap = {
+  xAxis: {
+    data: ['Clear', 'Colored'],
+    axisLine: {
+      show: false // Remove the x-axis line
+    },
+    splitLine: {
+      show: false // Remove the x-axis grid lines
+    }
+  },
+  yAxis: {
+    max: 100, // Set max to 100 for percentage scale
+    axisLine: {
+      show: false // Remove the y-axis line
+    },
+    splitLine: {
+      show: false, // Remove the y-axis grid lines
+    },
+    axisLabel: {
+      show: false // Remove the y-axis labels (100, 80, 60, etc.)
+    }
+  },
+  dataGroupId: '',
+  animationDurationUpdate: 500,
+  series: {
+    type: 'bar',
+    id: 'sales',
+    data: [
+      {
+        value: 30,
+        groupId: 'clear',
+        name: 'Clear',
+        itemStyle: {
+          color: '#00FFFF' // Green color for "Clear"
         }
-      };
-
-      optionBotlcap && myChartBotlcap.setOption(optionBotlcap);
-
-      var chartDomMain1 = document.getElementById('main1');
-      if (chartDomMain1) {
-        var chartMain1 = echarts.init(chartDomMain1);
-        var optionMain1 = {
-          backgroundColor: '#2c343c',
-          title: {
-            text: 'Customized Pie',
-            left: 'center',
-            top: 20,
-            textStyle: {
-              color: '#ccc'
-            }
-          },
-          tooltip: {
-            trigger: 'item'
-          },
-          series: [{
-            name: 'Access From',
-            type: 'pie',
-            radius: '55%',
-            center: ['50%', '50%'],
-            data: [{
-                value: 335,
-                name: 'Direct'
-              },
-              {
-                value: 310,
-                name: 'Email'
-              },
-              {
-                value: 274,
-                name: 'Union Ads'
-              },
-              {
-                value: 235,
-                name: 'Video Ads'
-              },
-              {
-                value: 400,
-                name: 'Search Engine'
-              }
-            ].sort(function(a, b) {
-              return a.value - b.value;
-            }),
-            roseType: 'radius',
-            label: {
-              color: 'rgba(255, 255, 255, 0.3)'
-            },
-            labelLine: {
-              lineStyle: {
-                color: 'rgba(255, 255, 255, 0.3)'
-              },
-              smooth: 0.2,
-              length: 10,
-              length2: 20
-            },
-            itemStyle: {
-              color: '#c23531',
-              shadowBlur: 200,
-              shadowColor: 'rgba(0, 0, 0, 0.5)'
-            },
-            animationType: 'scale',
-            animationEasing: 'elasticOut',
-            animationDelay: function(idx) {
-              return Math.random() * 200;
-            }
-          }]
-        };
-
-        chartMain1.setOption(optionMain1);
-      } else {
-        console.error("Element with id 'main1' not found!");
+      },
+      {
+        value: 20,
+        groupId: 'colored',
+        name: 'Colored',
+        itemStyle: {
+          color: 'green' // Green color for "Colored"
+        }
       }
+    ],
+    label: {
+      show: true,
+      position: 'top',
+      formatter: '{c}%', // Display value with percentage symbol
+      color: 'BLACK',
+      fontSize: 12
+    },
+    universalTransition: {
+      enabled: true,
+      divideShape: 'clone'
+    }
+  }
+};
+
+optionBotlcap && myChartBotlcap.setOption(optionBotlcap);
+
+
+//ECHARTS 2
+
+
+      var chartDom = document.getElementById('main1');
+var myChart = echarts.init(chartDom);
+var option;
+
+option = {
+  title: {
+    text: 'STORGE CAPACITY',
+    subtext: '1000 Capacity',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: '50%',
+      data: [
+        { value: 600, name: 'Cleared Bottle' },
+        { value: 345, name: 'Colored Bottle' },
+        { value: 55, name: 'Available Storage' },
+      
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+};
+
+option && myChart.setOption(option);
+
     </script>
 </body>
 
--- smartbin_db.sql
+++ smartbin_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2024 at 06:55 AM
+-- Generation Time: Dec 05, 2024 at 04:01 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,12 +24,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `transaction_history`
+--
+
+CREATE TABLE `transaction_history` (
+  `id` int(199) NOT NULL,
+  `user_id` varchar(199) NOT NULL,
+  `pts_earned` decimal(65,0) DEFAULT NULL,
+  `bottle_quantity` int(199) DEFAULT NULL,
+  `pts_deducted` decimal(65,0) DEFAULT NULL,
+  `acq_items` varchar(199) DEFAULT NULL,
+  `item_qty` int(5) DEFAULT NULL,
+  `service` enum('Vendo','Printer','','') DEFAULT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaction_history`
+--
+
+INSERT INTO `transaction_history` (`id`, `user_id`, `pts_earned`, `bottle_quantity`, `pts_deducted`, `acq_items`, `item_qty`, `service`, `timestamp`) VALUES
+(1, '20214365', 50, 10, NULL, NULL, NULL, NULL, '2024-11-28 06:30:00'),
+(2, '20214365', 30, 6, NULL, NULL, NULL, NULL, '2024-11-28 07:00:00'),
+(3, '20214365', 20, 4, 0, '', 0, NULL, '2024-11-28 08:00:00'),
+(4, '20214325', 30, 10, 0, '', 0, NULL, '2024-11-28 06:30:00'),
+(5, '20214361', 30, 6, 0, '', 0, NULL, '2024-11-28 07:00:00'),
+(6, '20214365', 20, 4, 0, '', 0, NULL, '2024-11-28 08:00:00'),
+(7, '20214343', 5, 10, 0, '', 0, NULL, '2024-11-28 06:30:00'),
+(8, '20214343', 3, 6, 0, '', 0, 'Vendo', '2024-11-28 07:00:00'),
+(9, '20214343', 20, 4, 0, '', 0, 'Vendo', '2024-11-28 08:00:00'),
+(10, '20214343', 50, 10, 0, '', 0, 'Vendo', '2024-11-29 02:59:24'),
+(11, '20214365', 0, 0, 10, 'Pen', 34, 'Vendo', '2024-11-29 07:52:34'),
+(12, '20214365', 0, 0, 110, 'Id Lace', 34, 'Printer', '2024-11-29 07:53:35');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` int(199) NOT NULL,
-  `user_id` int(8) NOT NULL,
+  `user_id` varchar(12) NOT NULL,
   `email` varchar(199) NOT NULL,
   `password` varchar(199) NOT NULL,
   `fname` varchar(199) NOT NULL,
@@ -37,8 +73,11 @@ CREATE TABLE `users` (
   `lname` varchar(199) NOT NULL,
   `suffix` enum('Jr.','Sr.','II','III','IV','') DEFAULT NULL,
   `fullname` varchar(199) NOT NULL,
-  `department` enum('BSCPE','BSECE','BSEE','BSME','BSCE','BSARCH') NOT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `department` enum('BSCPE','BSECE','BSEE','BSME','BSCE','BSARCH') DEFAULT NULL,
   `restriction` enum('SUPERADMIN','ADMIN','USER','') NOT NULL DEFAULT 'USER',
+  `status` enum('ACTIVE','LOCKED','INACTIVE','') NOT NULL DEFAULT 'ACTIVE',
+  `points` int(199) NOT NULL DEFAULT 0,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -46,19 +85,28 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `user_id`, `email`, `password`, `fname`, `mname`, `lname`, `suffix`, `fullname`, `department`, `restriction`, `created_at`) VALUES
-(1, 20214365, 'Email@gmail.com', '$2y$10$R13exV/Hqp.MNkqb7Mjjn.tdJkLeps0ibTEQ7vGnSUt93L441TKNq', 'John ', 'Cee', 'Nuh', 'Sr.', 'John Cee Nuh Sr.', 'BSECE', 'USER', '2024-11-12 05:55:19');
+INSERT INTO `users` (`id`, `user_id`, `email`, `password`, `fname`, `mname`, `lname`, `suffix`, `fullname`, `photo`, `department`, `restriction`, `status`, `points`, `created_at`) VALUES
+(1, '20214365', 'Easd123mail@gmail.coma', '$2y$10$R13exV/Hqp.MNkqb7Mjjn.tdJkLeps0ibTEQ7vGnSUt93L441TKNq', 'Markyggb', 'Cee', 'Nuh', 'II', 'Markyggb Cee Nuh II', 'uploads/profile_pictures/20214365_1733205965.jpg', NULL, 'SUPERADMIN', 'ACTIVE', 12, '2024-11-12 05:55:19'),
+(2, '20214343', 'Joemama@gmail.com', '$2y$10$LpavXn1sXnkUXGG1oGxtFOPw8dA7616zMsuXkSG2CYYO9DUHhZfyK', 'Joe', '', 'Mama', 'IV', 'Joe Mama IV', NULL, 'BSECE', 'USER', 'ACTIVE', 0, '2024-11-12 06:41:00'),
+(4, '20213434', 'joemama1@gmail.com', '$2y$10$F20aka1xtHE.xie7m6XLC.cGQ1FqEbwjS6P7coCR2wWz.sD49gqCG', 'Joe', 'Ma', 'Ma', 'II', 'Joe Ma Ma II', NULL, 'BSEE', 'ADMIN', 'INACTIVE', 0, '2024-11-12 06:42:55'),
+(5, '20212121', 'a;sldhaisdj@asijdoi', '$2y$10$2nxyx8ut4LD8a87A/VlTFOQZCmcfi0odBpbOuZP1fD/gmKc1JV3L6', 'asdsd', 'asdsd', 'asd', 'Sr.', 'asdsd asdsd asd Sr.', NULL, 'BSCPE', 'USER', 'ACTIVE', 0, '2024-11-27 12:36:53');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `transaction_history`
+--
+ALTER TABLE `transaction_history`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `user_id` (`user_id`),
+  ADD UNIQUE KEY `student_id` (`user_id`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
@@ -66,10 +114,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `transaction_history`
+--
+ALTER TABLE `transaction_history`
+  MODIFY `id` int(199) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(199) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(199) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
